5bc2b0277167bf51d76eba73d07cec041172c341
Wenyi <135293807+wen9yi@users.noreply.github.com>
2024-05-27 21:20:57 +0800

    Update GameScreenController.java

--- COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/GameScreenController.java
+++ COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/GameScreenController.java
@@ -2,13 +2,60 @@ package com.example.comp2008j_group13_majong;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
-public class GameScreenController {
-    @FXML
-    private Label welcomeText;
+import javax.swing.*;
+import java.awt.event.MouseListener;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class GameScreenController extends JLabel {
+    private String name;
+
+    private int value;
+
+    private boolean up;
+
+    private boolean canClick=false;
+
+    private boolean isClicked=false;
+
+    public GameScreenController(String name, int value, boolean up) {
+        this.name = name;
+        this.value = value;
+        this.up = up;
+
+        if(this.up){
+            //正面
+            turnFront();
+        }else {
+            //反面
+            turnRear();
+        }
+
+        this.setSize(12,16);
+        this.setVisible(true);
+        this.addMouseListener((MouseListener) this);
     }
+
+    public void turnFront(){
+        this.setIcon(new ImageIcon("src/main/resources/images/"+name+".jpg"));
+        this.up=true;
+    }
+
+    public void turnRear(){
+        this.setIcon(new ImageIcon("src/main/resources/images/背面.png"));
+        this.up=false;
+    }
+    public void mouseClicked(MouseEvent e) {
+        if(canClick){
+            if(isClicked){
+                //取消选中
+                isClicked=false;
+            }else {
+                //选中
+                isClicked=true;
+            }
+        }
+    }
+
+
 }
