48c0a4574610b8200ed6c518f9aeb0955a20311c
Wenyi <16601151855@163.com>
2024-05-28 11:26:42 +0800

    11

---
+++ COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/BeginScreenController.java
@@ -0,0 +1,25 @@
+package com.example.comp2008j_group13_majong;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class BeginScreenController {
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button newGame;
+
+    @FXML
+    void exitBottonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newGameBottonAction(ActionEvent event) {
+
+    }
+
+}
