0959c7e158d90dd6b033aa6636b7fdc9cbb43fb2
boran <a@a.com>
2024-05-28 13:43:12 +0800

    create controller folder

---
+++ COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/MasterControll/Deck.java
@@ -0,0 +1,5 @@
+package com.example.comp2008j_group13_majong.MasterControll;
+
+public class Deck {
+    //管理发牌洗牌
+}
---
+++ COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/MasterControll/TurnManager.java
@@ -0,0 +1,5 @@
+package com.example.comp2008j_group13_majong.MasterControll;
+
+public class TurnManager {
+    //管理玩家出牌顺序
+}
