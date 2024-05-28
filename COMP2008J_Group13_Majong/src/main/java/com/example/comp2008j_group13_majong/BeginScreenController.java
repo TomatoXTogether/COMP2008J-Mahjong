840af0ea2c8792d1b63b19931a73d733c40fd01e
Wenyi <16601151855@163.com>
2024-05-28 11:46:47 +0800

    11

--- COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/BeginScreenController.java
+++ COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/BeginScreenController.java
@@ -2,7 +2,13 @@ package com.example.comp2008j_group13_majong;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class BeginScreenController {
 
@@ -14,12 +20,21 @@ public class BeginScreenController {
 
     @FXML
     void exitBottonAction(ActionEvent event) {
-
+        Stage currentStage = (Stage) exit.getScene().getWindow();
+        currentStage.close();
     }
 
     @FXML
-    void newGameBottonAction(ActionEvent event) {
-
+    void newGameBottonAction(ActionEvent event) throws IOException {
+        Stage currentStage = (Stage) newGame.getScene().getWindow();
+        currentStage.close();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GameScreen.fxml"));
+        Parent root;
+        root = loader.load();
+        GameScreenController controller = loader.getController();
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+        newStage.show();
     }
 
 }
