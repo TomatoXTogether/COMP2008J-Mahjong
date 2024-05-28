d8e67b4b4cae9131d24c1e52a5efc1a30c23bd49
Blanca1117 <jiayi.cai@ucdconnect.ie>
2024-05-28 19:36:27 +0800

    Modify MahjongDeck.java

--- COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/Tile/MahjongTile.java
+++ COMP2008J_Group13_Majong/src/main/java/com/example/comp2008j_group13_majong/Tile/MahjongTile.java
@@ -1,20 +1,66 @@
 package com.example.comp2008j_group13_majong.Tile;
 
-public class MahjongTile {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MahjongTile extends JLabel implements MouseListener {
     public enum Suit {
         万, 条, 饼, 风, 发财, 白板
     }
 
+    // 牌的花色
     private Suit suit;
+    // 牌的值
     private String value;
+    // 牌显示正面还是背面
+    private boolean up;
+    // 是否可点击
+    private boolean canClick = false;
+    // 当前状态，是否已经被点击
+    private boolean clicked = false;
 
     public MahjongTile(Suit suit, String value) {
         this.suit = suit;
         this.value = value;
+        this.up = false;
+        setTileAppearance();
+        // 给每一张牌添加鼠标监听
+        this.addMouseListener(this);
+        // 设置牌的宽高大小
+        this.setSize(71, 96);
+        // 显示牌
+        this.setVisible(true);
     }
 
     public MahjongTile(Suit suit) {
-        this.suit = suit;
+        this(suit, null);
+    }
+
+    // 根据牌的状态（正面或背面）设置外观
+    private void setTileAppearance() {
+        if (this.up) {
+            this.turnFront();
+        } else {
+            this.turnRear();
+        }
+    }
+
+    // 显示正面
+    public void turnFront() {
+        if (value != null) {
+            this.setIcon(new ImageIcon("path_to_images/" + suit + "_" + value + ".png"));
+        } else {
+            this.setIcon(new ImageIcon("path_to_images/" + suit + ".png"));
+        }
+        this.up = true;
+    }
+
+    // 显示背面
+    public void turnRear() {
+        this.setIcon(new ImageIcon("path_to_images/rear.png")); // 修改图片路径
+        this.up = false;
     }
 
     public Suit getSuit() {
@@ -25,12 +71,73 @@ public class MahjongTile {
         return value;
     }
 
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+        setTileAppearance();
+    }
+
+    public boolean isCanClick() {
+        return canClick;
+    }
+
+    public void setCanClick(boolean canClick) {
+        this.canClick = canClick;
+    }
+
+    public boolean isClicked() {
+        return clicked;
+    }
+
+    public void setClicked(boolean clicked) {
+        this.clicked = clicked;
+    }
+
+    @Override
     public String toString() {
         if (value != null) {
             return value + " " + suit;
         } else {
             return String.valueOf(suit);
         }
+    }
+
+    // 出牌时，需要点击牌
+    // 被点击之后，牌向上移动20个像素
+    // 再次被点击，牌回落20个像素
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (canClick) {
+            Point from = this.getLocation();
+            int step;
+            if (clicked) {
+                step = 20;
+            } else {
+                step = -20;
+            }
+            clicked = !clicked;
+            Point to = new Point(from.x, from.y + step);
+            this.setLocation(to);
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
     }
 
 }
